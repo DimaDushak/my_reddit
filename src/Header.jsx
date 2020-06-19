ba40bfca8ecab89bb3ad1d209a4456c2bfc470ae
DimaDushak <dushakdima1991@gmail.com>
2020-06-19 19:31:53 +0300

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import styles from './header.scss';
+
+export const Header = () => {
+    return (
+        <header>
+            <h1 className={styles.example}>Hello, World!</h1>
+        </header>
+    );
+}
